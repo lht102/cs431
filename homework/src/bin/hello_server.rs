a899ea38af5e9d5b1ee6aebb9539650e6e78e4a7
Tony Lau <lht102.p@gmail.com>
2022-12-05 02:47:13 +0800

    shorten println

--- homework/src/bin/hello_server.rs
+++ homework/src/bin/hello_server.rs
@@ -9,10 +9,7 @@ fn main() -> io::Result<()> {
     // Use a browser that doesn't cache too eagerly so that request is always sent. For example,
     // Firefox works well.  If you want to test using command line only, use curl. If you want to
     // run it on the lab server, you may need to change the port number to something else.
-    println!(
-        "Run `curl http://{}/KEY` to query the server with KEY",
-        ADDR
-    );
+    println!("Run `curl http://{ADDR}/KEY` to query the server with KEY");
 
     // The thread pool.
     //
